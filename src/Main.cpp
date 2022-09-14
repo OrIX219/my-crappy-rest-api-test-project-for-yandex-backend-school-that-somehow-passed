f74d739a1e52c3d25abcb3546f996ff9d60ea8c7
OrIX219 <artem-kraev00@mail.ru>
2022-09-14 17:36:10 +0300

    get db name and user from argv

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <iostream>
 #include <string>
 #include <unordered_set>
@@ -6,9 +7,7 @@
 #include "DbManager.h"
 #include "Utils.h"
 
-static DbManager dbm("rest", "orix");
-
-bool ImportsValidate(const json& req) {
+bool ImportsValidate(DbManager& dbm, const json& req) {
   if (!req.contains("items")
     || !req.contains("updateDate"))
     return false;
@@ -60,18 +59,18 @@ bool ImportsValidate(const json& req) {
   return true;
 }
 
-void ChangeSize(const std::string& id, int delta, const std::string& date) {
+void ChangeSize(DbManager& dbm, const std::string& id, int delta, const std::string& date) {
   std::string parent = db::GetParent(dbm, id);
   if (!parent.empty())
-    ChangeSize(parent, delta, date);
+    ChangeSize(dbm, parent, delta, date);
   db::ChangeSize(dbm, id, delta);
   db::SetDate(dbm, id, date);
   json updatedFolder = db::GetItem(dbm, id);
   db::InsertIntoHistory(dbm, updatedFolder);
 }
 
-void ImportsHandler(const Request& req, Reply& rep) {
-  if (!ImportsValidate(req.body)) {
+void ImportsHandler(DbManager& dbm, const Request& req, Reply& rep) {
+  if (!ImportsValidate(dbm, req.body)) {
     rep = Reply::StockReply(Reply::bad_request);
     return;
   }
@@ -97,18 +96,18 @@ void ImportsHandler(const Request& req, Reply& rep) {
     if (old_parent != parent) {
       size_delta = item["size"].get<int>();
       if (!old_parent.empty())
-        ChangeSize(old_parent, -item["size"].get<int>(), date);
+        ChangeSize(dbm, old_parent, -item["size"].get<int>(), date);
     }
     if (!parent.empty())
-      ChangeSize(parent, size_delta, date);
+      ChangeSize(dbm, parent, size_delta, date);
   }
   rep.status = Reply::ok;
 }
 
-int DeleteIter(const std::string& id) {
+int DeleteIter(DbManager& dbm, const std::string& id) {
   int deleted_size = 0;
   for (const std::string& child_id : db::GetChildren(dbm, id))
-    deleted_size += DeleteIter(child_id);
+    deleted_size += DeleteIter(dbm, child_id);
   bool is_file = db::GetType(dbm, id) == "FILE";
   int cur_size = db::DeleteItem(dbm, id);
   if (is_file)
@@ -118,7 +117,7 @@ int DeleteIter(const std::string& id) {
   return deleted_size;
 }
 
-void DeleteHandler(const Request& req, Reply& rep) {
+void DeleteHandler(DbManager& dbm, const Request& req, Reply& rep) {
   if (req.params.count("date") == 0
     || !utils::IsDateValid(req.params.at("date"))) {
     rep = Reply::StockReply(Reply::bad_request);
@@ -132,19 +131,19 @@ void DeleteHandler(const Request& req, Reply& rep) {
   }
 
   std::string parent = db::GetParent(dbm, id);
-  int deleted_size = DeleteIter(id);
+  int deleted_size = DeleteIter(dbm, id);
   if (!parent.empty())
-    ChangeSize(parent, -deleted_size, req.params.at("date"));
+    ChangeSize(dbm, parent, -deleted_size, req.params.at("date"));
 
   rep.status = Reply::ok;
 }
 
-json NodesIter(const std::string& id) {
+json NodesIter(DbManager& dbm, const std::string& id) {
   json item = db::GetItem(dbm, id);
   if (item["type"].get<std::string>() == "FOLDER") {
     item["children"] = json::value_t::array;
     for (const std::string& child : db::GetChildren(dbm, id))
-      item["children"].push_back(NodesIter(child));
+      item["children"].push_back(NodesIter(dbm, child));
   } else {
     item["children"] = json::value_t::null;
   }
@@ -152,7 +151,7 @@ json NodesIter(const std::string& id) {
   return item;
 }
 
-void NodesHandler(const Request& req, Reply& rep) {
+void NodesHandler(DbManager& dbm, const Request& req, Reply& rep) {
   std::string id = 
     req.uri.substr(req.uri.find_first_of("/") + 1);
   utils::StripSpaces(id);
@@ -161,12 +160,12 @@ void NodesHandler(const Request& req, Reply& rep) {
     return;
   }
 
-  json item = NodesIter(id);
+  json item = NodesIter(dbm, id);
   rep.content = item.dump(2, ' ');
   rep.status = Reply::ok;
 }
 
-void UpdatesHandler(const Request& req, Reply& rep) {
+void UpdatesHandler(DbManager& dbm, const Request& req, Reply& rep) {
   if (req.params.count("date") == 0
     || !utils::IsDateValid(req.params.at("date"))) {
     rep = Reply::StockReply(Reply::bad_request);
@@ -185,7 +184,7 @@ void UpdatesHandler(const Request& req, Reply& rep) {
   rep.status = Reply::ok;
 }
 
-void HistoryHandler(const Request& req, Reply& rep) {
+void HistoryHandler(DbManager& dbm, const Request& req, Reply& rep) {
   size_t id_start = req.uri.find_first_of("/") + 1;
   std::string id = req.uri.substr(
     id_start, req.uri.find_last_of("/") - id_start);
@@ -214,21 +213,34 @@ void HistoryHandler(const Request& req, Reply& rep) {
 
 int main(int argc, char* argv[]) {
   try {
-    if (argc != 3) {
+    if (argc != 5) {
       std::cerr << "Usage:\n";
-      std::cerr << "\tMain <address> <port>\n";
+      std::cerr << "\tMain <address> <port> <db name> <db user>\n";
       return 1;
     }
+
+    DbManager dbm(argv[3], argv[4]);
 
     db::CreateItemTable(dbm);
     db::CreateHistoryTable(dbm);
 
     RequestHandler request_handler;
-    request_handler.SetHandler("imports", ImportsHandler);
-    request_handler.SetHandler("delete", DeleteHandler);
-    request_handler.SetHandler("nodes", NodesHandler);
-    request_handler.SetHandler("updates", UpdatesHandler);
-    request_handler.SetHandler("node", HistoryHandler);
+    request_handler.SetHandler("imports", 
+      std::bind(ImportsHandler, std::ref(dbm), 
+      std::placeholders::_1, std::placeholders::_2));
+    request_handler.SetHandler("delete", 
+      std::bind(DeleteHandler, std::ref(dbm), 
+      std::placeholders::_1, std::placeholders::_2));
+    request_handler.SetHandler("nodes", 
+      std::bind(NodesHandler, std::ref(dbm), 
+      std::placeholders::_1, std::placeholders::_2));
+    request_handler.SetHandler("updates", 
+      std::bind(UpdatesHandler, std::ref(dbm), 
+      std::placeholders::_1, std::placeholders::_2));
+    request_handler.SetHandler("node", 
+      std::bind(HistoryHandler, std::ref(dbm), 
+      std::placeholders::_1, std::placeholders::_2));
+      
     Server s(argv[1], argv[2], std::move(request_handler));
 
     s.Run();
